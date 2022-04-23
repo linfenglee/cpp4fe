5dd0da4367e82f052870fdd1c699aae94c93faa1
linfengli <linfengli.larry@icloud.com>
2022-04-23 13:53:13 +0800

    Initial commit

---
+++ lec1/hello_world.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include "hello_world.h"
+
+// function definition
+void print_hello_world(){
+    std::cout << "Hello World" << std::endl;
+}
+
---
+++ lec1/main.cpp
@@ -0,0 +1,8 @@
+#include "hello_world.h"
+
+// entry function to the executable
+// int main(int argc, char** argv)
+int main(){
+    print_hello_world();
+    return 0;
+}
